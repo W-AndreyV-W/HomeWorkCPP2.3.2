1ffe9871b23d483d3b66f3f3f82d8dba4b7ab645
Andrey V <Andreyxvv@hotmail.com>
2023-01-28 22:43:45 +0500

    first commit

---
+++ HomeWorkCPP2.3.2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Counter {
+public:
+    std::string sign;
+    int firstnum = 0;
+    void first();
+    bool end();
+    void plus();
+    void minus();
+    void print();
+private:
+    int num = 1;
+};
+
+void Counter::first() {
+    if (sign == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> firstnum;
+        num = firstnum;
+    }
+}
+
+bool Counter::end() {
+    if (sign == "х") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void Counter::plus() {
+    if (sign == "+") {
+        num++;
+    }
+}
+
+void Counter::minus() {
+    if (sign == "-") {
+        num--;
+    }
+}
+
+void Counter::print() {
+    if (sign == "=") {
+        std::cout << num << std::endl;
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Counter signs;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> signs.sign;
+    signs.first();
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> signs.sign;
+        signs.plus();
+        signs.minus();
+        signs.print();
+    } while (signs.end());
+}
